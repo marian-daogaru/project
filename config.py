860af613ef8d1d968019f16a332547dd9b40b994
marian-daogaru <marian@phyramid.com>
2018-01-03 15:57:35 +0200

    initial commit

---
+++ config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+# mail server settings
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'mdaogaru.test'
+MAIL_PASSWORD = 'MarianDaogar9'
+
+# administrator list
+ADMINS = ['marian@phyramid.com']
