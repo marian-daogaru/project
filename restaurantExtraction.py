43c5d95d84b471069b1e65fdec807ae4638fa2c7
marian-daogaru <marian@phyramid.com>
2018-01-04 15:51:08 +0200

    added data extraction for Caserola + Oliviera and for any random website. For a random website, it will take the name from the title, and it will take a snap of the frontal webpage. THis could be modified, to take a screenshot of just a part of the website, but cba...

--- restaurantExtraction.py
+++ restaurantExtraction.py
@@ -1,12 +1,23 @@
-import urllib2, urllib
-link = 'https://www.hipmenu.ro/kfc-iulius-cluj?c=00DMAOKMG1TB4M&src=hipMenu&l=ro'
+import urllib2
+import urllib
+import ssl
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+link = 'https://www.caserola.ro/restaurant/zexezahana'
 
 mediaFolderPath = 'app/data/media/avatars/restaurants/'
 
 def openWebsite(link):
     opener = urllib2.build_opener()
-    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    opener.addheaders = [('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A')]
     return opener.open(link)
+
+
+def openWebsiteCert(link):
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+    return urllib2.urlopen(link, context=ctx)
 
 
 def saveImage(imageName, imageLink):
@@ -24,11 +35,31 @@ def extractFoodPanda(link):
     httpStart = webpage[bannerIndex:].index("https://") + bannerIndex
     httpStop = webpage[httpStart + 10:].index("https://") + httpStart + 9  # 10 - 1 due to | at the end
     avatarLink = webpage[httpStart : httpStop]
-    avatarName = mediaFolderPath + '{}.jpg'.format("".join(name.split()).lower())
+    avatarName = mediaFolderPath + '{}.png'.format("".join(name.split()).lower())
 
     saveImage(avatarName, avatarLink)
 
     return (name, avatarName)
+
+
+def extractOliviera(link):
+    webpage = openWebsite(link).read()
+    nameIndex = webpage.index('meta property="og:title" content="') + 34
+    firstBraketIndex = webpage[nameIndex:].index('>') + nameIndex - 1  # exclude the "
+    name = webpage[nameIndex : firstBraketIndex]
+
+    httpStart = firstBraketIndex + 42  # \n\t\t<meta property="og:image" content=" length
+    httpStop = webpage[httpStart:].index('">') + httpStart
+    avatarLink = webpage[httpStart : httpStop]
+    avatarName = mediaFolderPath + '{}.png'.format("".join(name.split()).lower())
+
+
+    # this is different because we are using certificate authentification
+    with open(avatarName, 'wb') as myfile:
+        myfile.write(openWebsiteCert(avatarLink).read())
+
+    return name, avatarName
+
 
 def extractHipMenu(link):
     webpage = openWebsite(link).read()
@@ -36,15 +67,53 @@ def extractHipMenu(link):
     nameIndexStop = webpage[nameIndexStart:].index("</span>") + nameIndexStart
     name = webpage[nameIndexStart : nameIndexStop]
 
-    bannerIndex = webpage.index('id="logoInfo"')
-    httpStart = webpage[bannerIndex:].index("https://") + bannerIndex
+    logoIndex = webpage.index('id="logoInfo"')
+    httpStart = webpage[logoIndex:].index("https://") + logoIndex
     httpStop = webpage[httpStart:].index('">') + httpStart
     avatarLink = webpage[httpStart : httpStop]
-    avatarName = mediaFolderPath + '{}.jpg'.format("".join(name.split()).lower())
+    avatarName = mediaFolderPath + '{}.png'.format("".join(name.split()).lower())
 
     saveImage(avatarName, avatarLink)
-    
-    return name, avatarLink, avatarName
-#
+
+    return name, avatarName
+
+
+def extractCaserola(link):
+    webpage = openWebsite(link).read()
+    nameIndexStart = webpage.index('h1 itemprop="name"') + 19
+    nameIndexStop = webpage[nameIndexStart:].index('</h1>') + nameIndexStart
+    name = webpage[nameIndexStart : nameIndexStop]
+
+    logoIndex = webpage.index('restaurant-logo')
+    httpStart = webpage[logoIndex:].index("/images") + logoIndex
+    httpStop = webpage[httpStart:].index('" alt=') + httpStart
+    avatarLink = 'https://caserola.ro' + webpage[httpStart : httpStop]
+    avatarName = mediaFolderPath + '{}.png'.format("".join(name.split()).lower())
+
+    with open(avatarName, 'wb') as myfile:
+        myfile.write(openWebsite(avatarLink).read())
+
+    return name, avatarLink
+
+
+def captureImage(link, imageName):
+    driver = webdriver.PhantomJS()
+    driver.set_window_size(1024, 768)
+    driver.get(link)
+    driver.save_screenshot(imageName)
+    driver.close()
+
+def extractParticular(link):
+    webpage = openWebsite(link).read()
+    nameIndexStart = webpage.index('<title>') + 7
+    nameIndexStop = webpage[nameIndexStart:].index('</title>') + nameIndexStart - 1
+    name = webpage[nameIndexStart : nameIndexStop]
+
+    avatarName = mediaFolderPath + '{}.png'.format("".join(name.split()).lower())
+    captureImage(link, avatarName)
+    return name
+
 # with open('url.txt', 'w') as myfile:
 #     myfile.write(openWebsite(link).read())
+
+print(extractCaserola(link))
